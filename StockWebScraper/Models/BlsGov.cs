eb5358b7be94e7a47505c94828b5e49ffe4e0714
Juraj Glavas <p3502872@ness.com>
2021-02-18 18:01:26 +0100

    UnemploymentStart

---
+++ StockWebScraper/Models/BlsGov.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockWebScraper.Models
+{
+    class BlsGov
+    {
+    }
+
+    public class ActualData
+    {
+        [JsonProperty("year")]
+        public string Year { get; set; }
+
+        [JsonProperty("period")]
+        public string Period { get; set; }
+
+        [JsonProperty("periodName")]
+        public string PeriodName { get; set; }
+
+        [JsonProperty("value")]
+        public string Value { get; set; }       
+    }
+
+    public class Series
+    {
+        [JsonProperty("seriesID")]
+        public string SeriesID { get; set; }
+
+        [JsonProperty("data")]
+        public List<ActualData> Data { get; set; }
+    }
+
+    public class Results
+    {
+        [JsonProperty("series")]
+        public List<Series> Series { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("responseTime")]
+        public int ResponseTime { get; set; }
+
+        [JsonProperty("message")]
+        public List<string> Message { get; set; }
+
+        [JsonProperty("Results")]
+        public Results Results { get; set; }
+    }
+}
---
+++ StockWebScraper/Models/SeriesPost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockWebScraper.Models
+{
+    public class SeriesPost
+    {
+        public string[] seriesid { get; set; }
+        public string startyear { get; set; }
+        public string endyear { get; set; }       
+        public string registrationKey { get; set; }
+    }
+}
